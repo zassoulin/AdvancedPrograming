598b1e34a0ece7fdcad29d5399826ac94f7a462a
ziv <zassoulin@gmail.com>
2023-03-29 20:41:07 +0300

    Initial commit

---
+++ src/main/java/com/ap/advancedprograming/AdvancedprogramingApplication.java
@@ -0,0 +1,13 @@
+package com.ap.advancedprograming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedprogramingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedprogramingApplication.class, args);
+	}
+
+}
