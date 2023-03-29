2440a658982bb495506ab23393fef961b0455fd0
ziv <zassoulin@gmail.com>
2023-03-29 20:42:03 +0300

    nothing creating develop

--- src/test/java/com/ap/advancedprograming/AdvancedprogramingApplicationTests.java
+++ src/test/java/com/ap/advancedprograming/AdvancedprogramingApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AdvancedprogramingApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
