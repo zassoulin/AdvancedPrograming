8e01fc16fe5d2afa06d2d7a44af7cd16cf4220a5
GalGR <GalGR@users.noreply.github.com>
2023-05-17 02:55:27 +0300

    Add sample test file to both projects.
    We should use this as template on how to create real tests for
    our classes (per the project's requirements).

---
+++ dictionary_server/src/test/java/ap/scrabble/dictionary_server/SampleTest.java
@@ -0,0 +1,18 @@
+package ap.scrabble.dictionary_server;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SampleTest {
+	@Test
+	public void testAssertTrue() {
+		assertTrue("".isEmpty());
+	}
+
+	@Test
+	public void testAssertEquals() {
+		assertEquals(2, 1 + 1);
+	}
+}
